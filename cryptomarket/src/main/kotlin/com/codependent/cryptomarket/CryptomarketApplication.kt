13472f0a2eaa51db662434c66750ef3c236acf0e
Jose <jainigo@profile.es>
2018-03-17 21:38:32 +0100

    initial commit

---
+++ cryptomarket/src/main/kotlin/com/codependent/cryptomarket/CryptomarketApplication.kt
@@ -0,0 +1,11 @@
+package com.codependent.cryptomarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptomarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptomarketApplication>(*args)
+}
