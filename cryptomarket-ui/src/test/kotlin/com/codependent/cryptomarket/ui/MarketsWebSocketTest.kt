bece96704eb2c7f8978d671dd6850e020fc9e30c
jainigo <jainigo@profile.es>
2022-05-09 11:46:53 +0200

    upgrade

--- cryptomarket-ui/src/test/kotlin/com/codependent/cryptomarket/ui/MarketsWebSocketTest.kt
+++ cryptomarket-ui/src/test/kotlin/com/codependent/cryptomarket/ui/MarketsWebSocketTest.kt
@@ -6,6 +6,7 @@ import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClien
 import reactor.core.publisher.EmitterProcessor
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.Sinks
 import java.net.URI
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
@@ -18,50 +19,55 @@ class MarketsWebSocketTest {
     @Test
     fun shouldInvokeWebSocket() {
         val latch = CountDownLatch(20)
-        val output = EmitterProcessor.create<String>()
-        val client = ReactorNettyWebSocketClient()
 
-        val input = Mono.just("BTC").doOnNext { println(it) }
+        val sink = Sinks.many().multicast().onBackpressureBuffer<String>()
+        val client = ReactorNettyWebSocketClient()
+        val input = Mono.just("BTC").doOnNext { logger.info("on next {}", it) }
 
         val sessionMono = client.execute(URI.create("ws://localhost:8080/markets")) { session ->
             session.send(input.map {
                 session.textMessage(it)
             }).thenMany(session.receive()
-                    .map { it.payloadAsText }
-                    .subscribeWith(output).then()).then()
+                .map { it.payloadAsText })
+                .doOnNext { sink.tryEmitNext(it) }
+                .then()
         }
 
-        output.doOnSubscribe { _ -> sessionMono.subscribe() }
-                .doOnNext { c ->
-                    logger.info("Received [{}]", c)
-                    latch.countDown()
-                }.subscribe()
+        val sinkFlux = sink.asFlux()
+        sinkFlux
+            .doOnNext { c ->
+                logger.info("Received [{}]", c)
+                latch.countDown()
+            }
+            .doOnSubscribe { sessionMono.subscribe() }
+            .subscribe()
 
         latch.await()
     }
 
+    /*
     @Test
     fun myTest() {
         val latch = CountDownLatch(20)
         val output = EmitterProcessor.create<String>()
         val input = Flux.generate<String> { sink -> sink.next("This is a test") }
-                .delayElements(Duration.ofSeconds(1))
+            .delayElements(Duration.ofSeconds(1))
 
         val client = ReactorNettyWebSocketClient()
         val sessionMono = client.execute(URI.create("ws://echo.websocket.org")) { session ->
             session.send(input.map {
                 session.textMessage(it)
             }).thenMany(session.receive()
-                    .map { it.payloadAsText }
-                    .subscribeWith(output).then()).then()
+                .map { it.payloadAsText }
+                .subscribeWith(output).then()).then()
         }
 
         output.doOnSubscribe { _ -> sessionMono.subscribe() }
-                .doOnNext { c ->
-                    logger.info("Received [{}]", c)
-                    latch.countDown()
-                }.subscribe()
+            .doOnNext { c ->
+                logger.info("Received [{}]", c)
+                latch.countDown()
+            }.subscribe()
 
         latch.await()
-    }
+    }*/
 }
